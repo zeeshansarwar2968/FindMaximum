ef8580b248c3604d529300fd467295f1de03a096
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-22 15:30:39 +0530

    [Zeeshan] : Added get maximum string value program

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace FindMaximum
         {
             Console.WriteLine("\t\t\t\t\tFind Maximum Problem using Generics");
             Console.WriteLine("");
-            Console.WriteLine("1.Compare three integer numbers \n2.Compare three float numbers");
+            Console.WriteLine("1.Compare three integer numbers \n2.Compare three float numbers \n3.Compare three string values");
             Console.WriteLine("");
             Console.WriteLine("\t\t\t\t\t    Please enter 0 to exit");
             Console.WriteLine("");
@@ -33,6 +33,12 @@ namespace FindMaximum
                         Console.WriteLine(FloatMaximum.MaxFloatNumber(10.2f, 20.5f, 20.5f));
                         Console.WriteLine(FloatMaximum.MaxFloatNumber(60f, 20.9f, 100.0f));
                         Console.WriteLine(FloatMaximum.MaxFloatNumber(55.5f, 45.5f, 1.1f));
+                        break;
+                    case 3:
+                        Console.WriteLine("Executing three test cases for Finding Maximum String Value");
+                        Console.WriteLine(StringMaximum.MaxStringValue("Abc", "Abc1", "Abcc"));
+                        Console.WriteLine(StringMaximum.MaxStringValue("Aabbcc", "Abc123", "Abcc"));
+                        Console.WriteLine(StringMaximum.MaxStringValue("Abbc", "Abc", "AAbc"));
                         break;
                     default:
                         Console.WriteLine("Invalid option/entry, please choose again.");
---
+++ StringMaximum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    internal class StringMaximum
+    {
+        public static string MaxStringValue(string string1, string string2, string string3)
+        {
+            //Condition to Compare the string1 to string2 and string3
+            if (string1.CompareTo(string2) > 0 && string1.CompareTo(string3) > 0 || string1.CompareTo(string2) >= 0 && string1.CompareTo(string3) > 0 || string1.CompareTo(string2) > 0 && string1.CompareTo(string3) >= 0)
+            {
+                return string1;
+            }
+            //Condition to Compare the string2 to string1 and string3
+            if (string2.CompareTo(string1) > 0 && string2.CompareTo(string3) > 0 || string2.CompareTo(string1) >= 0 && string2.CompareTo(string3) > 0 || string2.CompareTo(string1) > 0 && string2.CompareTo(string3) >= 0)
+            {
+                return string2;
+            }
+            //Condition to Compare the string3 to string2 and string1
+            if (string3.CompareTo(string1) > 0 && string3.CompareTo(string2) > 0 || string3.CompareTo(string1) >= 0 && string3.CompareTo(string2) > 0 || string3.CompareTo(string1) > 0 && string3.CompareTo(string2) >= 0)
+            {
+                return string3;
+            }
+            //return value if all the strings are equal
+            return string1;
+        }
+    }
+}
