3a0da3d083f4731d571f3cdf4133bea3c888ea51
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-22 15:00:25 +0530

    [Zeeshan] : Get max integer value program

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace FindMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t\tFind Maximum Problem using Generics");
+            Console.WriteLine("");
+            //Printing three test cases
+            Console.WriteLine(IntegerMaximum.MaxIntegerNumber(20, 20, 20));
+            Console.WriteLine(IntegerMaximum.MaxIntegerNumber(65, 20, 10));
+            Console.WriteLine(IntegerMaximum.MaxIntegerNumber(5, 45, 98));
         }   
     }
 }
