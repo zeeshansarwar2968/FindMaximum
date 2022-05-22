261140e6e2d7a43d1b787e9c0a86881e6355242b
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-22 15:19:08 +0530

    [Zeeshan] : Added get maximum float value program

---
+++ FloatMaximum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    internal class FloatMaximum
+    {
+        public static float MaxFloatNumber(float num1, float num2, float num3)
+        {
+            //Condition to Compare the num1 to num2 and num3
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            //Condition to Compare the num2 to num1 and num3
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 || num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            //Condition to Compare the num3 to num2 and num1
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 || num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 || num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            //return value if all the numbers are equal
+            return num1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,10 +8,39 @@ namespace FindMaximum
         {
             Console.WriteLine("\t\t\t\t\tFind Maximum Problem using Generics");
             Console.WriteLine("");
-            //Printing three test cases
-            Console.WriteLine(IntegerMaximum.MaxIntegerNumber(20, 20, 20));
-            Console.WriteLine(IntegerMaximum.MaxIntegerNumber(65, 20, 10));
-            Console.WriteLine(IntegerMaximum.MaxIntegerNumber(5, 45, 98));
+            Console.WriteLine("1.Compare three integer numbers \n2.Compare three float numbers");
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t\t    Please enter 0 to exit");
+            Console.WriteLine("");
+
+            int flag = 0;
+
+            while (flag == 0)
+            {
+                Console.Write("\nPlease choose the program to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0: flag = 1; break;
+                    case 1:
+                        Console.WriteLine("Executing three test cases for Finding Maximum Integer Value");
+                        Console.WriteLine(IntegerMaximum.MaxIntegerNumber(20, 20, 20));
+                        Console.WriteLine(IntegerMaximum.MaxIntegerNumber(65, 20, 10));
+                        Console.WriteLine(IntegerMaximum.MaxIntegerNumber(5, 45, 98));
+                        break;
+                    case 2:
+                        Console.WriteLine("Executing three test cases for Finding Maximum Float Value");
+                        Console.WriteLine(FloatMaximum.MaxFloatNumber(10.2f, 20.5f, 20.5f));
+                        Console.WriteLine(FloatMaximum.MaxFloatNumber(60f, 20.9f, 100.0f));
+                        Console.WriteLine(FloatMaximum.MaxFloatNumber(55.5f, 45.5f, 1.1f));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option/entry, please choose again.");
+                        break;
+                }
+            }
+              
+            
         }   
     }
 }
