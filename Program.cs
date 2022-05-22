a47e97f42c6c9a4d091855374a89b5dfd0728892
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-22 11:57:58 +0530

    [Zeeshan] : Main page Construction

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximum 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tFind Maximum Problem using Generics");
+        }   
+    }
+}
